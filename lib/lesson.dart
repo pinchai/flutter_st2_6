6f79514a81be05e0cf59b6373d108c8db230c804
Pinchai <>
2024-07-19 10:07:01 +0700

    finished var

--- lib/lesson.dart
+++ lib/lesson.dart
@@ -1,3 +1,21 @@
 void main(){
-  print("Hello world");
+  // int x = 2;
+  // double y = 2.7;
+  // bool s = true;
+  // bool s1 = false;
+  // String name = 'kaka';
+  // var p = 5;
+  // var a = 10.5;
+  // dynamic b = 12;
+  // b = 12.5;
+  // b = true;
+  // b = 'kaka';
+  // b = [];
+  // b = {};
+  // final email = "chai@gmail.com";
+  // // Same as var but cannot be reassigned
+  // const qty = 5; // Compile-time constant
+  // print(email.runtimeType);
+
+
 }
